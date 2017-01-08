24f7e6fa714b87270888e87caddb5673cc1d631b
kaneko <kaneko@kaneko.local>
2017-01-08 23:17:19 +0900

    scss変換とgulp-sassに変更、gulp-autoprefixerを追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,39 @@
-var gulp = require('gulp');	//gulp
-var plumber = require('gulp-plumber');	//watch dont stop
-var browserSync = require('browser-sync');  //browser live reload
-var sass = require('gulp-ruby-sass');		//scss -> css
-var ejs = require('gulp-ejs');		//include
+var gulp         = require('gulp');               //gulp
+var ejs          = require('gulp-ejs');           //include
+var sass         = require('gulp-sass');          //scss -> css
+var plumber      = require('gulp-plumber');       //watch dont stop
+var browserSync  = require('browser-sync');       //browser live reload
+var autoprefixer = require('gulp-autoprefixer');  //autoprefixer
 
 //copy to html/js/font/img
-gulp.task('copy', function() {
+gulp.task('copy', function(){
 	gulp.src('src/js/**', { base: 'src'}).pipe( gulp.dest('dist') );
 	gulp.src('src/img/**', { base: 'src'}).pipe( gulp.dest('dist') );
-	gulp.src('src/favicons/**', { base: 'src'}).pipe( gulp.dest('dist') );
-	gulp.src('src/font/**', { base: 'src'}).pipe( gulp.dest('dist') );
+	// gulp.src('src/favicons/**', { base: 'src'}).pipe( gulp.dest('dist') );
+	// gulp.src('src/font/**', { base: 'src'}).pipe( gulp.dest('dist') );
 });
 
-gulp.task('ejs', function() {
+gulp.task('ejs', function(){
 	gulp.src(['./src/**.html', '!./src/_*.ejs'])
 		.pipe(plumber())
-		.pipe(ejs({}, {ext: '.html'}))
-		.pipe(gulp.dest('./dist/'))
+		.pipe(ejs('', {'ext': '.html'}))
+		.pipe(gulp.dest('./dist/'));
 });
 
 //scss -> css
-gulp.task('sass', function() {
-	return sass('src/css/*.scss', {style: 'compressed'})	//expanded,compressed,nested
+gulp.task('sass', function(){
+	gulp.src('./src/css/*.scss')
 		.pipe(plumber())
-		.pipe(gulp.dest('dist/css'));
+		.pipe(sass({outputStyle: 'compressed'}))	//expanded,compressed,nested
+		.pipe(autoprefixer({
+	        browsers: ['last 2 versions'],
+	        cascade: false
+	    }))
+		.pipe(gulp.dest('./dist/css'));
 });
 
 //watching files
-gulp.task('watch', function() {
+gulp.task('watch', function(){
 	gulp.watch('src/**/*.html', ['ejs']);
 	gulp.watch('src/js/**', ['copy']);
 	gulp.watch('src/img/**', ['copy']);
@@ -40,19 +46,19 @@ gulp.task('watch', function() {
 });
 
 //browserSync reload
-gulp.task('bs-reload', function () {
+gulp.task('bs-reload', function(){
 	browserSync.reload();
 });
 
 //webserver display(browserSync)
-gulp.task('webserver', function() {
-	browserSync({
-		notify: false,
-		port: 8000,
-		server: {
-			baseDir: 'dist'
-		}
-	})
+gulp.task('webserver', function(){
+	return browserSync.init({
+	notify: false,
+	port: 8000,
+    server: {
+      baseDir: './dist/'
+    }
+  })
 });
 
 gulp.task('default', ['copy', 'ejs', 'sass', 'watch', 'webserver']);
