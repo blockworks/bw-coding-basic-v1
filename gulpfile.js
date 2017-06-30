8a73d1d68d190e55f484b944b52de4c5720c3d0a
Kenichi Kaneko <blockworks2@gmail.com>
2017-06-30 11:09:30 +0900

    構成を大幅に改定。

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,51 @@
-var gulp         = require('gulp');               //gulp
-var ejs          = require('gulp-ejs');           //include
-var sass         = require('gulp-sass');          //scss -> css
-var plumber      = require('gulp-plumber');       //watch dont stop
-var browserSync  = require('browser-sync');       //browser live reload
-var autoprefixer = require('gulp-autoprefixer');  //autoprefixer
-
-//copy to html/js/font/img
-gulp.task('copy', function(){
-	gulp.src('src/js/**', { base: 'src'}).pipe( gulp.dest('dist') );
-	gulp.src('src/img/**', { base: 'src'}).pipe( gulp.dest('dist') );
-	// gulp.src('src/favicons/**', { base: 'src'}).pipe( gulp.dest('dist') );
-	// gulp.src('src/font/**', { base: 'src'}).pipe( gulp.dest('dist') );
-});
-
-gulp.task('ejs', function(){
-	gulp.src(['./src/**.html', '!./src/_*.ejs'])
-		.pipe(plumber())
-		.pipe(ejs('', {'ext': '.html'}))
-		.pipe(gulp.dest('./dist/'));
-});
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass         = require('gulp-sass');	//scss -> css
+var plumber      = require('gulp-plumber');	//watch dont stop
+var autoprefixer = require('gulp-autoprefixer');	//autoprefixer
 
 //scss -> css
 gulp.task('sass', function(){
-	gulp.src('./src/css/*.scss')
+	gulp.src('./css/**/*.scss')
 		.pipe(plumber())
 		.pipe(sass({outputStyle: 'compressed'}))	//expanded,compressed,nested
 		.pipe(autoprefixer({
-	        browsers: ['last 2 versions'],
+	        browsers: ['last 3 versions'],
 	        cascade: false
 	    }))
-		.pipe(gulp.dest('./dist/css'));
+		.pipe(gulp.dest('./css/'));
+		
+		gulp.src('./js/**/*.scss')
+		.pipe(plumber())
+		.pipe(sass({outputStyle: 'compressed'}))	//expanded,compressed,nested
+		.pipe(autoprefixer({
+	        browsers: ['last 3 versions'],
+	        cascade: false
+	    }))
+		.pipe(gulp.dest('./js/'));
 });
 
-//watching files
-gulp.task('watch', function(){
-	gulp.watch('src/**/*.html', ['ejs']);
-	gulp.watch('src/js/**', ['copy']);
-	gulp.watch('src/img/**', ['copy']);
-	gulp.watch('src/**/*.scss', ['sass']);
-	gulp.watch('src/**/*.ejs', ['ejs']);
-	//reload
-	gulp.watch('dist/**/*.html', ['bs-reload']);
-	gulp.watch('dist/**/*.css', ['bs-reload']);
-	gulp.watch('dist/**/*.js', ['bs-reload']);
-});
-
-//browserSync reload
-gulp.task('bs-reload', function(){
-	browserSync.reload();
-});
-
-//webserver display(browserSync)
-gulp.task('webserver', function(){
+//browserSync
+gulp.task('browser-sync', function() {
 	browserSync({
 		notify: false,
-		port: 8000,
 		server: {
-			baseDir: './dist',       //対象ディレクトリ
-			index  : 'index.html'      //インデックスファイル
+			baseDir: "./"	//対象ディレクトリ
+			,index  : "index.html"	//インデックスファイル
 		}
 	});
 });
 
-gulp.task('default', ['copy', 'ejs', 'sass', 'watch', 'webserver']);
+//browser reload
+gulp.task('bs-reload', function () {
+	browserSync.reload();
+});
+
+//watching files
+gulp.task('default', ['sass', 'browser-sync'], function () {
+	gulp.watch("./css/**/*.scss", ['sass']);
+	gulp.watch("./*.html", ['bs-reload']);
+	gulp.watch("./css/**/*.css", ['bs-reload']);
+	gulp.watch("./js/**/*.js", ['bs-reload']);
+	gulp.watch("./js/**/*.css", ['bs-reload']);
+});
